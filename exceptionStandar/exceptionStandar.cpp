7beca2704798c5649a4ae71737a282dececb1c94
nrismawardah <nrismawardah@gmail.com>
2023-06-19 11:27:02 +0700

    Membuat penanda 1

---
+++ exceptionStandar/exceptionStandar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <exception>
+// Untuk objek exception yang akan digunakan
+#include <array>
+// Untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main() {
+	cout << "Awal Program" << endl;		// Penanda 1:...
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		// Pesan array integer 3 elemen
+		// cout << data.at(5) << endl;
+		// Memamnggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//Penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*Akan dieksekusi karena array data hanya memiliki 3 elemen*/
+	}
+}
