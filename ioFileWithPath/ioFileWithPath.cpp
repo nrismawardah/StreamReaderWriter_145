5e685c139ab3ea688cbddb21295d57e6469bd374
nrismawardah <nrismawardah@gmail.com>
2023-06-19 10:55:25 +0700

    Membuat fungsi membuka dan menunjuk file

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -6,4 +6,12 @@ using namespace std;
 int main() {
 	string baris;
 	string NamaFile;
+
+	cout << "Masukkan Nama File : ";
+	cin >> NamaFile;
+
+	// Membuka file dalam mode menulis
+	ofstream outfile;
+	// Menunjuk ke sebuah nama file
+	outfile.open(NamaFile, ios::out);
 }
