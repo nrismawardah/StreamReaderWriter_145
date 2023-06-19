555e917625d0d084667837253bb5d528ca40881e
nrismawardah <nrismawardah@gmail.com>
2023-06-19 11:00:21 +0700

    Membuat unlimited loop untuk menulis

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -14,4 +14,17 @@ int main() {
 	ofstream outfile;
 	// Menunjuk ke sebuah nama file
 	outfile.open(NamaFile, ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	// Unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		// Mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		// Loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		// Menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
